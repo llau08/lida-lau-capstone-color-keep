bef7a95009c1a51b0bc1b259a38d23c0ad358529
llau08 <llau251@gmail.com>
2021-12-07 08:36:55 -0800

    added caption to carousel image

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -24,6 +24,8 @@ function Home() {
     {
       image:
         "https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80",
+
+      caption: "Done by Brian",
     },
   ];
 
