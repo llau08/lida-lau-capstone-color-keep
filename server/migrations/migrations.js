64ab2e287e276ba03aa985b451db149f7287d4bf
llau08 <llau251@gmail.com>
2021-12-07 09:12:43 -0800

    corrected spelling error

--- server/migrations/migrations.js
+++ server/migrations/migrations.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     table.increments("id");
     table.string("firstName").notNullable();
     table.string("lastName").notNullable();
-    table.integer("phone").notNullable();
+    table.string("phone").notNullable();
     table.string("email").notNullable();
     table.string("stylist").notNullable();
     table.string("dateVisited").notNullable();
